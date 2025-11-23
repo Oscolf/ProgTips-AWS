871f590226f57bdea4da75c7793ea3e15e8777a1
Oscolf <oscarvr.110503@gmail.com>
2025-11-22 18:53:05 -0600

    Login v1.0

--- scripts/usuarioContrasena.js
+++ scripts/usuarioContrasena.js
@@ -7,4 +7,21 @@ function validarUsuario() {
   console.log("Contraseña: " + password);
 
   alert("¡Hola " + user + ", ha accedido corrrectamente a ProgTips!");
+
+  // se muestra el usuario y la contraseña en elementos HTML
+
+  userElement = document.createElement("p");
+  passwordElement = document.createElement("p");
+
+  userElement.textContent = "Usuario: " + user;
+
+  userInfoDiv = document.createElement("div");
+  userInfoDiv.style.border = "1px solid black";
+  userInfoDiv.style.padding = "10px";
+  userInfoDiv.style.marginTop = "10px";
+
+  userInfoDiv.appendChild(userElement);
+
+  loginContainer = document.getElementsByClassName("login");
+  loginContainer[0].appendChild(userInfoDiv);
 }
